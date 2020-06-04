efee87888416f718e7799f1802f79412807e00c5
Emanuele Bertuol <emanuelebertuol@gmail.com>
2020-06-04 14:55:58 +0200

    WarningSign

--- src/components/WarningSign.jsx
+++ src/components/WarningSign.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import {Alert} from 'react-bootstrap';
 
 class WarningSign extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <Alert variant={'danger'}>
+                {this.props.text}
+            </Alert>
         );
     }
 }
 
 export default WarningSign;
+
+
+
+/*
+Create a component WarningSign which receives as a property a text. This text should be presented inside an.
+Alert of type danger
+*/

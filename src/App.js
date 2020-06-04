13f197bef959882a5c34cf3b7e53a102bc681d96
Emanuele Bertuol <emanuelebertuol@gmail.com>
2020-06-04 15:31:17 +0200

    SingleBook

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import './App.css';
 import WarningSign from './components/WarningSign'
 import MyBadge from './components/MyBadge'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import SingleBook from './components/SingleBook';
 
 function App() {
   return (
     <div className="App">
       <WarningSign text="Some Text" />
       <MyBadge text="badge text" variant="danger" />
+      <SingleBook 
+        asin="1621579832"
+        title="Justice on Trial: The Kavanaugh Confirmation and the Future of the Supreme Court"
+        img="https://images-na.ssl-images-amazon.com/images/I/71WdFQNYSJL.jpg"
+        price={17.39}
+        category="history"
+      />
     </div>
   );
 }
--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
 class SingleBook extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="col">
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={this.props.img} />
+                    <Card.Body>
+                        <Card.Title>
+                            {this.props.title}
+                        </Card.Title>
+                        <Card.Text>
+                        <div className="d-flex justify-content-around">
+                            <div>
+                                {this.props.category}
+                            </div>
+                            <div>
+                                €{this.props.price}
+                            </div>                            
+                        </div>
+                        <div>
+                        <Button variant="primary">Add To Cart</Button> 
+                        </div>                                                    
+                        </Card.Text>                        
+                    </Card.Body>
+                </Card>                                    
             </div>
         );
     }
 }
 
 export default SingleBook;
+
+
+/*
+Create a SingleBook component. The component receives as a prop a book object and display the cover and the
+title of the book. Use react-bootstrap Cards to display a book (The book object can be read from the one of the .json
+book files)
+*/

e9becdf69058c03732ebc7c25ac6a3974d5454e4
Emanuele Bertuol <emanuelebertuol@gmail.com>
2020-06-04 16:40:41 +0200

    Filter (to be finished)

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import SingleBook from './SingleBook';
+import {InputGroup, DropdownButton, FormControl, Dropdown} from 'react-bootstrap';
 
 let books = {
     fantasy: require("../data/fantasy.json"),
@@ -11,15 +12,34 @@ let books = {
     scifi: require("../data/scifi.json")
 }
 
+
+
 class BookList extends Component {
     render() {
         // let _books = books["fantasy"]
         let _books = books.fantasy
-        // console.log(_books)
+
+        let _categories = Object.keys(books)
+        console.log(_categories)
         // return <></>
 
         return (
             <Container>
+                <Row>
+                    <InputGroup className="mb-3">
+                        <DropdownButton
+                        as={InputGroup.Prepend}
+                        variant="outline-secondary"
+                        title="Categories"
+                        id="input-group-dropdown-1"
+                        >
+                            {
+                                _categories.map(category => <Dropdown.Item href="#">{category}</Dropdown.Item>)
+                            }
+                        </DropdownButton>
+                        <FormControl aria-describedby="basic-addon1" />
+                    </InputGroup>
+                </Row>
                 <Row className="row-cols-1 row-cols-md-3 row-cols-lg-5">
                     {
                         _books.map( book => <SingleBook book={ book } />)
--- src/components/FilterBookList.jsx
+++ src/components/FilterBookList.jsx
@@ -11,3 +11,8 @@ class FilterBookList extends Component {
 }
 
 export default FilterBookList;
+
+/*
+Create a FilterBookList feature. By writing into a textbook a search query, the BookList should show only the
+books with the specified string into the title #HINT: filter on the props.
+*/

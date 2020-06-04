c3ee0899976898965c2494781e214c4d022d3568
Emanuele Bertuol <emanuelebertuol@gmail.com>
2020-06-04 14:43:03 +0200

    Inizial

---
+++ src/components/SingleBook.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SingleBook extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default SingleBook;
---
+++ src/components/WarningSign.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class WarningSign extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default WarningSign;
